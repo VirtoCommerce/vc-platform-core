ee49ee017e4bf56056e55b2f5e0d001c894a4bda
Andrey Kostyrin <kostyrin2@gmail.com>
2018-05-28 10:27:12 +0200

    #39 JobsController (#44)

---
+++ VirtoCommerce.Platform.Core/Jobs/Job.cs
@@ -0,0 +1,9 @@
+namespace VirtoCommerce.Platform.Core.Jobs
+{
+    public class Job
+    {
+        public string Id { get; set; }
+        public string State { get; set; }
+        public bool Completed { get; set; }
+    }
+}
