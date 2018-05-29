906aac8221a0396de4079ce24ebe7fd9e4ae5199
n2pro <prohorov.nn@gmail.com>
2018-05-29 20:56:01 +0200

    Description filter attribute

--- VirtoCommerce.Platform.Web/Filters/DisableFormValueModelBindingAttribute.cs
+++ VirtoCommerce.Platform.Web/Filters/DisableFormValueModelBindingAttribute.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace VirtoCommerce.Platform.Web.Helpers
 {
+    /// <summary>
+    /// This attribute is used to disable model binding for the upload action
+    /// More information https://docs.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-2.1
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
     {
