e49b4ca98464333ff6bfb8d6cc6e1c8639a0eac4
Andrey Kostyrin <kostyrin2@gmail.com>
2018-05-31 15:50:59 +0200

    reset password notification (#40)
    * #28 Add to platform abstraction to be able to send email notifications
    
    * #28 ResetPasswordEmailNotification
    
    * #28 NotificationEmailSender implementation
    
    * #28 NotificationEmailSender simple implementation
    
    * #28 refactor after review
    
    * #28 move EmailSendingOptions and refactor projects
    
    * #28 NotificationsExportImportManagerIntegrationTests
    
    * #28 refactor options
    
    * #28 refactor options
    
    * #28 options move to implementation projects

--- Modules/vc-module-notifications/VirtoCommerce.NotificationsModule.Tests/IntegrationTests/NotificationSenderIntegrationTests.cs
+++ Modules/vc-module-notifications/VirtoCommerce.NotificationsModule.Tests/IntegrationTests/NotificationSenderIntegrationTests.cs
@@ -17,6 +17,7 @@ using VirtoCommerce.NotificationsModule.Tests.Model;
 using VirtoCommerce.NotificationsModule.Tests.NotificationTypes;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Notifications;
 using Xunit;
 
 namespace VirtoCommerce.NotificationsModule.Tests.IntegrationTests
@@ -26,31 +27,28 @@ namespace VirtoCommerce.NotificationsModule.Tests.IntegrationTests
         INotificationMessageSender _messageSender;
         NotificationSender _notificationSender;
         private readonly Mock<INotificationService> _serviceMock;
-        private readonly INotificationTemplateRender _templateRender;
+        private readonly INotificationTemplateRenderer _templateRender;
         private readonly Mock<INotificationMessageService> _messageServiceMock;
-        private readonly Mock<IOptions<EmailSendingOptions>> _emailSendingOptionsMock;
+        private readonly Mock<IOptions<SmtpSenderOptions>> _emailSendingOptionsMock;
         private readonly INotificationRegistrar _notificationRegistrar;
         private readonly Mock<INotificationRepository> _repositoryMock;
         private readonly Mock<ILogger<NotificationSender>> _logNotificationSenderMock;
         private readonly Mock<IEventPublisher> _eventPulisherMock;
         private INotificationMessageSenderProviderFactory _notificationMessageSenderProviderFactory;
-        private readonly EmailSendingOptions _emailSendingOptions;
+        private readonly SmtpSenderOptions _emailSendingOptions;
 
         public NotificationSenderIntegrationTests()
         {
-            _emailSendingOptions = new EmailSendingOptions()
+            _emailSendingOptions = new SmtpSenderOptions()
             {
-                SmtpOptions = new SmtpOptions()
-                {
                     SmtpServer = "smtp.gmail.com",
                     Port = 587,
                     Login = "tasker.for.test@gmail.com",
                     Password = "FLGxiJQc"
-                }
             };
             _templateRender = new LiquidTemplateRenderer();
             _messageServiceMock = new Mock<INotificationMessageService>();
-            _emailSendingOptionsMock = new Mock<IOptions<EmailSendingOptions>>();
+            _emailSendingOptionsMock = new Mock<IOptions<SmtpSenderOptions>>();
             _serviceMock = new Mock<INotificationService>();
             _repositoryMock = new Mock<INotificationRepository>();
             _logNotificationSenderMock = new Mock<ILogger<NotificationSender>>();
@@ -109,7 +107,7 @@ namespace VirtoCommerce.NotificationsModule.Tests.IntegrationTests
             _messageSender = new SmtpEmailNotificationMessageSender(_emailSendingOptionsMock.Object);
             _notificationMessageSenderProviderFactory = new NotificationMessageSenderProviderFactory(new List<INotificationMessageSender>() { _messageSender });
             _notificationMessageSenderProviderFactory.RegisterSenderForType<EmailNotification, SmtpEmailNotificationMessageSender>();
-            _notificationSender = new NotificationSender(_serviceMock.Object, _templateRender, _messageServiceMock.Object, _logNotificationSenderMock.Object, _notificationMessageSenderProviderFactory);
+            _notificationSender = new NotificationSender(_templateRender, _messageServiceMock.Object, _logNotificationSenderMock.Object, _notificationMessageSenderProviderFactory);
 
             //Act
             var result = await _notificationSender.SendNotificationAsync(notification, null);
@@ -143,10 +141,10 @@ namespace VirtoCommerce.NotificationsModule.Tests.IntegrationTests
 
             _serviceMock.Setup(serv => serv.GetByTypeAsync(nameof(OrderSentEmailNotification), null, null, null)).ReturnsAsync(notification);
 
-            _emailSendingOptions.SmtpOptions.Password = "wrong_password";
+            _emailSendingOptions.Password = "wrong_password";
             _emailSendingOptionsMock.Setup(opt => opt.Value).Returns(_emailSendingOptions);
             _messageSender = new SmtpEmailNotificationMessageSender(_emailSendingOptionsMock.Object);
-            _notificationSender = new NotificationSender(_serviceMock.Object, _templateRender, _messageServiceMock.Object, _logNotificationSenderMock.Object,
+            _notificationSender = new NotificationSender(_templateRender, _messageServiceMock.Object, _logNotificationSenderMock.Object,
                 _notificationMessageSenderProviderFactory);
 
             //Act
