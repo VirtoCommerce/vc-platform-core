e53161a4b2755f625df2565c3e3ee8dc6de620c0
Andrey Kostyrin <kostyrin2@gmail.com>
2018-06-14 09:04:21 +0200

    notification twitter extension (#62)

--- Modules/vc-module-notifications/VirtoCommerce.NotificationsSampleModule.Web/Models/TwitterNotification.cs
+++ Modules/vc-module-notifications/VirtoCommerce.NotificationsSampleModule.Web/Models/TwitterNotification.cs
@@ -4,6 +4,7 @@ namespace VirtoCommerce.NotificationsSampleModule.Web.Models
 {
     public class TwitterNotification : Notification
     {
+        public string Post { get; set; }
         public TwitterNotification()
         {
             Kind = nameof(TwitterNotification);
