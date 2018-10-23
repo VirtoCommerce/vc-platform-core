1aa9ed4e1da7d9960da4fd8fa9ec6213516d338b
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-10-23 19:55:41 +0700

    ShoppingCartSearchServiceImpl: made retrieving carts from database, criteria application and SortInfos generation overridable.

--- Modules/vc-module-cart/VirtoCommerce.CartModule.Data/Services/ShoppingCartSearchServiceImpl.cs
+++ Modules/vc-module-cart/VirtoCommerce.CartModule.Data/Services/ShoppingCartSearchServiceImpl.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -16,93 +17,111 @@ namespace VirtoCommerce.CartModule.Data.Services
 {
     public class ShoppingCartSearchServiceImpl : IShoppingCartSearchService
     {
-        private readonly Func<ICartRepository> _repositoryFactory;
-        private readonly IPlatformMemoryCache _platformMemoryCache;
-        private readonly IShoppingCartService _cartService;
-
         public ShoppingCartSearchServiceImpl(Func<ICartRepository> repositoryFactory, IPlatformMemoryCache platformMemoryCache, IShoppingCartService cartService)
         {
-            _repositoryFactory = repositoryFactory;
-            _platformMemoryCache = platformMemoryCache;
-            _cartService = cartService;
+            RepositoryFactory = repositoryFactory;
+            PlatformMemoryCache = platformMemoryCache;
+            CartService = cartService;
         }
+
+        protected Func<ICartRepository> RepositoryFactory { get; }
+        protected IPlatformMemoryCache PlatformMemoryCache { get; }
+        protected IShoppingCartService CartService { get; }
 
         public async Task<GenericSearchResult<ShoppingCart>> SearchCartAsync(ShoppingCartSearchCriteria criteria)
         {
             var retVal = new GenericSearchResult<ShoppingCart>();
             var cacheKey = CacheKey.With(GetType(), "SearchCartAsync", criteria.GetCacheKey());
-            return await _platformMemoryCache.GetOrCreateExclusiveAsync(cacheKey, async (cacheEntry) =>
+            return await PlatformMemoryCache.GetOrCreateExclusiveAsync(cacheKey, async cacheEntry =>
             {
                 cacheEntry.AddExpirationToken(CartSearchCacheRegion.CreateChangeToken());
-                using (var repository = _repositoryFactory())
+                using (var repository = RepositoryFactory())
                 {
-                    var query = repository.ShoppingCarts;
-
-                    if (!string.IsNullOrEmpty(criteria.Status))
-                    {
-                        query = query.Where(x => x.Status == criteria.Status);
-                    }
-
-                    if (!string.IsNullOrEmpty(criteria.Name))
-                    {
-                        query = query.Where(x => x.Name == criteria.Name);
-                    }
-
-                    if (!string.IsNullOrEmpty(criteria.CustomerId))
-                    {
-                        query = query.Where(x => x.CustomerId == criteria.CustomerId);
-                    }
-
-                    if (!string.IsNullOrEmpty(criteria.StoreId))
-                    {
-                        query = query.Where(x => criteria.StoreId == x.StoreId);
-                    }
-
-                    if (!string.IsNullOrEmpty(criteria.Currency))
-                    {
-                        query = query.Where(x => x.Currency == criteria.Currency);
-                    }
-
-                    if (!string.IsNullOrEmpty(criteria.Type))
-                    {
-                        query = query.Where(x => x.Type == criteria.Type);
-                    }
-
-                    if (!string.IsNullOrEmpty(criteria.OrganizationId))
-                    {
-                        query = query.Where(x => x.OrganizationId == criteria.OrganizationId);
-                    }
-
-                    if (!criteria.CustomerIds.IsNullOrEmpty())
-                    {
-                        query = query.Where(x => criteria.CustomerIds.Contains(x.CustomerId));
-                    }
-
-                    var sortInfos = criteria.SortInfos;
-                    if (sortInfos.IsNullOrEmpty())
-                    {
-                        sortInfos = new[]
-                        {
-                            new SortInfo
-                            {
-                                SortColumn = ReflectionUtility.GetPropertyName<ShoppingCartEntity>(x => x.CreatedDate),
-                                SortDirection = SortDirection.Descending
-                            }
-                        };
-                    }
-
-                    query = query.OrderBySortInfos(sortInfos);
+                    var sortInfos = GetSortInfos(criteria);
+                    var query = GetQuery(repository, criteria, sortInfos);
 
                     retVal.TotalCount = await query.CountAsync();
                     if (criteria.Take > 0)
                     {
                         var cartIds = await query.Select(x => x.Id).Skip(criteria.Skip).Take(criteria.Take).ToArrayAsync();
-                        retVal.Results = (await _cartService.GetByIdsAsync(cartIds, criteria.ResponseGroup)).AsQueryable().OrderBySortInfos(sortInfos).ToArray();
+                        retVal.Results = (await CartService.GetByIdsAsync(cartIds, criteria.ResponseGroup)).AsQueryable().OrderBySortInfos(sortInfos).ToArray();
                     }
 
                     return retVal;
                 }
             });
+        }
+
+        protected virtual IQueryable<ShoppingCartEntity> GetQuery(ICartRepository repository, ShoppingCartSearchCriteria criteria, IEnumerable<SortInfo> sortInfos)
+        {
+            var query = GetQueryableShoppingCarts(repository);
+
+            if (!string.IsNullOrEmpty(criteria.Status))
+            {
+                query = query.Where(x => x.Status == criteria.Status);
+            }
+
+            if (!string.IsNullOrEmpty(criteria.Name))
+            {
+                query = query.Where(x => x.Name == criteria.Name);
+            }
+
+            if (!string.IsNullOrEmpty(criteria.CustomerId))
+            {
+                query = query.Where(x => x.CustomerId == criteria.CustomerId);
+            }
+
+            if (!string.IsNullOrEmpty(criteria.StoreId))
+            {
+                query = query.Where(x => criteria.StoreId == x.StoreId);
+            }
+
+            if (!string.IsNullOrEmpty(criteria.Currency))
+            {
+                query = query.Where(x => x.Currency == criteria.Currency);
+            }
+
+            if (!string.IsNullOrEmpty(criteria.Type))
+            {
+                query = query.Where(x => x.Type == criteria.Type);
+            }
+
+            if (!string.IsNullOrEmpty(criteria.OrganizationId))
+            {
+                query = query.Where(x => x.OrganizationId == criteria.OrganizationId);
+            }
+
+            if (!criteria.CustomerIds.IsNullOrEmpty())
+            {
+                query = query.Where(x => criteria.CustomerIds.Contains(x.CustomerId));
+            }
+
+            query.OrderBySortInfos(sortInfos);
+
+            return query;
+        }
+
+        protected virtual IQueryable<ShoppingCartEntity> GetQueryableShoppingCarts(ICartRepository repository)
+        {
+            return repository.ShoppingCarts;
+        }
+
+        protected virtual IList<SortInfo> GetSortInfos(ShoppingCartSearchCriteria criteria)
+        {
+            var sortInfos = criteria.SortInfos;
+            if (sortInfos.IsNullOrEmpty())
+            {
+                sortInfos = new[]
+                {
+                    new SortInfo
+                    {
+                        SortColumn = ReflectionUtility.GetPropertyName<ShoppingCartEntity>(x => x.CreatedDate),
+                        SortDirection = SortDirection.Descending
+                    }
+                };
+            }
+
+            return sortInfos;
         }
     }
 }
