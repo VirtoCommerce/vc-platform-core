35d47383bf3d0c09ed9ab4b2dc111077d0a7c0b2
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-10-24 23:02:06 +0700

    Added price from/till date support to services and DTOs.

--- Modules/vc-module-pricing/VirtoCommerce.PricingModule.Core/Model/Price.cs
+++ Modules/vc-module-pricing/VirtoCommerce.PricingModule.Core/Model/Price.cs
@@ -14,6 +14,18 @@ namespace VirtoCommerce.PricingModule.Core.Model
 		public decimal List { get; set; }
 		public int MinQuantity { get; set; }
 
+        /// <summary>
+        /// Optional start date for this price, so that we can prepare prices ahead of time.
+        /// If start date equals now, this price will be active.
+        /// </summary>
+        public DateTime? StartDate { get; set; }
+
+        /// <summary>
+        /// Optional end date for this price, so that we can prepare prices ahead of time.
+        /// If end date equals now, this price will not be active.
+        /// </summary>
+        public DateTime? EndDate { get; set; }
+
 		public decimal EffectiveValue
 		{
 			get
---
+++ Modules/vc-module-pricing/VirtoCommerce.PricingModule.Core/Services/IPricingChangeService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using VirtoCommerce.PricingModule.Core.Model;
+
+namespace VirtoCommerce.PricingModule.Core.Services
+{
+    /// <summary>
+    /// Service responsible for providing changes for the prices that are not caused by user changes.
+    /// </summary>
+    public interface IPricingChangeService
+    {
+        /// <summary>
+        /// Returns all price changes due to date filtering.
+        /// </summary>
+        /// <param name="lastEvaluationTimestamp">Last time this evaluation was called. 
+        /// If this parameter is null, beginning of time will be used.</param>
+        /// <param name="evaluationTimestamp">Moment to evaluate. If this parameter is null, 
+        /// current UTC time will be used.</param>
+        /// <param name="skip">Optional count of price changes to skip for the pagination.</param>
+        /// <param name="take">Optional count of price changes to take for the pagination.</param>
+        /// <returns></returns>
+        IEnumerable<PriceCalendarChange> GetCalendarChanges(DateTime? lastEvaluationTimestamp,
+            DateTime? evaluationTimestamp, int? skip = null, int? take = null);
+    }
+}
