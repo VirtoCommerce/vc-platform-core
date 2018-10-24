07802a2cb37e9dc1bceb4c245635770d9b357c17
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-10-24 19:04:21 +0700

    AddressType: fixed incorrect value for AddressType.Pickup item.

--- Modules/vc-module-core/VirtoCommerce.CoreModule.Core/Common/AddressType.cs
+++ Modules/vc-module-core/VirtoCommerce.CoreModule.Core/Common/AddressType.cs
@@ -8,7 +8,7 @@ namespace VirtoCommerce.CoreModule.Core.Common
         Undef = 0,
         Billing = 1,
         Shipping = 2,
-        Pickup = 3,
+        Pickup = 4,
         BillingAndShipping = Billing | Shipping
     }
 }
