862559fff5cdaad68b0b4f020c757f3f7de97955
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-10-30 12:56:30 +0700

    StoreService, StoreSearchService: replaced private fields with protected properties, made all methods public virtual or protected virtual.

--- Modules/vc-module-store/VirtoCommerce.StoreModule.Data/Services/StoreSearchService.cs
+++ Modules/vc-module-store/VirtoCommerce.StoreModule.Data/Services/StoreSearchService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -9,41 +10,33 @@ using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.StoreModule.Core.Model.Search;
 using VirtoCommerce.StoreModule.Core.Services;
 using VirtoCommerce.StoreModule.Data.Caching;
+using VirtoCommerce.StoreModule.Data.Model;
 using VirtoCommerce.StoreModule.Data.Repositories;
 
 namespace VirtoCommerce.StoreModule.Data.Services
 {
     public class StoreSearchService : IStoreSearchService
     {
-        private readonly Func<IStoreRepository> _repositoryFactory;
-        private readonly IPlatformMemoryCache _platformMemoryCache;
-        private readonly IStoreService _storeService;
-
         public StoreSearchService(Func<IStoreRepository> repositoryFactory, IPlatformMemoryCache platformMemoryCache, IStoreService storeService)
         {
-            _repositoryFactory = repositoryFactory;
-            _platformMemoryCache = platformMemoryCache;
-            _storeService = storeService;
+            RepositoryFactory = repositoryFactory;
+            PlatformMemoryCache = platformMemoryCache;
+            StoreService = storeService;
         }
 
-        public async Task<GenericSearchResult<Store>> SearchStoresAsync(StoreSearchCriteria criteria)
+        protected Func<IStoreRepository> RepositoryFactory { get; }
+        protected IPlatformMemoryCache PlatformMemoryCache { get; }
+        protected IStoreService StoreService { get; }
+
+        public virtual async Task<GenericSearchResult<Store>> SearchStoresAsync(StoreSearchCriteria criteria)
         {
             var cacheKey = CacheKey.With(GetType(), "SearchStoresAsync", criteria.GetCacheKey());
-            return await _platformMemoryCache.GetOrCreateExclusiveAsync(cacheKey, async (cacheEntry) =>
+            return await PlatformMemoryCache.GetOrCreateExclusiveAsync(cacheKey, async (cacheEntry) =>
             {
                 cacheEntry.AddExpirationToken(StoreSearchCacheRegion.CreateChangeToken());
                 var result = new GenericSearchResult<Store>();
-                using (var repository = _repositoryFactory())
+                using (var repository = RepositoryFactory())
                 {
-                    var query = repository.Stores;
-                    if (!string.IsNullOrEmpty(criteria.Keyword))
-                    {
-                        query = query.Where(x => x.Name.Contains(criteria.Keyword) || x.Id.Contains(criteria.Keyword));
-                    }
-                    if (!criteria.StoreIds.IsNullOrEmpty())
-                    {
-                        query = query.Where(x => criteria.StoreIds.Contains(x.Id));
-                    }
                     var sortInfos = criteria.SortInfos;
                     if (sortInfos.IsNullOrEmpty())
                     {
@@ -56,17 +49,34 @@ namespace VirtoCommerce.StoreModule.Data.Services
                         };
                     }
 
-                    query = query.OrderBySortInfos(sortInfos);
+                    var query = GetStoresQuery(repository, criteria, sortInfos);
 
                     result.TotalCount = await query.CountAsync();
                     if (criteria.Take > 0)
                     {
                         var storeIds = await query.Select(x => x.Id).Skip(criteria.Skip).Take(criteria.Take).ToArrayAsync();
-                        result.Results = (await _storeService.GetByIdsAsync(storeIds)).AsQueryable().OrderBySortInfos(sortInfos).ToList();
+                        result.Results = (await StoreService.GetByIdsAsync(storeIds)).AsQueryable().OrderBySortInfos(sortInfos).ToList();
                     }
                 }
                 return result;
             });
+        }
+
+        protected virtual IQueryable<StoreEntity> GetStoresQuery(IStoreRepository repository, StoreSearchCriteria criteria,
+            IEnumerable<SortInfo> sortInfos)
+        {
+            var query = repository.Stores;
+            if (!string.IsNullOrEmpty(criteria.Keyword))
+            {
+                query = query.Where(x => x.Name.Contains(criteria.Keyword) || x.Id.Contains(criteria.Keyword));
+            }
+            if (!criteria.StoreIds.IsNullOrEmpty())
+            {
+                query = query.Where(x => criteria.StoreIds.Contains(x.Id));
+            }
+
+            query = query.OrderBySortInfos(sortInfos);
+            return query;
         }
     }
 }
