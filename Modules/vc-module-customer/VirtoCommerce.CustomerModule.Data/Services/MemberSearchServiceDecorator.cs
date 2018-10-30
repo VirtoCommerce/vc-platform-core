0ba3bf5a6af6f525125fdfab33082d7c08bb5482
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-10-30 16:25:07 +0700

    All CustomerModule services: replaced private fields with protected readonly properties.

--- Modules/vc-module-customer/VirtoCommerce.CustomerModule.Data/Services/MemberSearchServiceDecorator.cs
+++ Modules/vc-module-customer/VirtoCommerce.CustomerModule.Data/Services/MemberSearchServiceDecorator.cs
@@ -9,27 +9,27 @@ namespace VirtoCommerce.CustomerModule.Data.Services
 {
     public class MemberSearchServiceDecorator : IMemberSearchService
     {
-        private readonly CommerceMembersSearchServiceImpl _memberSearchService;
-        private readonly MemberIndexedSearchService _memberIndexedSearchService;
-
         public MemberSearchServiceDecorator(CommerceMembersSearchServiceImpl memberSearchService, MemberIndexedSearchService memberIndexedSearchService)
         {
-            _memberSearchService = memberSearchService;
-            _memberIndexedSearchService = memberIndexedSearchService;
+            MemberSearchService = memberSearchService;
+            MemberIndexedSearchService = memberIndexedSearchService;
         }
+
+        protected CommerceMembersSearchServiceImpl MemberSearchService { get; }
+        protected MemberIndexedSearchService MemberIndexedSearchService { get; }
 
         public virtual Task<GenericSearchResult<Member>> SearchMembersAsync(MembersSearchCriteria criteria)
         {
             var result = !string.IsNullOrEmpty(criteria?.Keyword)
                 ? SearchIndexAsync(criteria)
-                : _memberSearchService.SearchMembersAsync(criteria);
+                : MemberSearchService.SearchMembersAsync(criteria);
 
             return result;
         }
 
         protected virtual Task<GenericSearchResult<Member>> SearchIndexAsync(MembersSearchCriteria criteria)
         {
-            return _memberIndexedSearchService.SearchAsync(criteria);
+            return MemberIndexedSearchService.SearchAsync(criteria);
         }
     }
 }
