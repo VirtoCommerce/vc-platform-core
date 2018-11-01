e22046c8fbf5f9c3de3fa2e66b35ed29857b3d5c
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-11-01 13:13:38 +0700

    ModuleConstants: added RestartRequired flag for settings that actually require restart.

--- Modules/vc-module-imageTools/VirtoCommerce.ImageToolsModule.Core/ModuleConstants.cs
+++ Modules/vc-module-imageTools/VirtoCommerce.ImageToolsModule.Core/ModuleConstants.cs
@@ -29,6 +29,7 @@ namespace VirtoCommerce.ImageToolsModule.Core
                     GroupName = "Thumbnail|General",
                     ValueType = SettingValueType.Boolean,
                     DefaultValue = false,
+                    RestartRequired = true
                 };
 
                 public static SettingDescriptor ImageProcessJobCronExpression = new SettingDescriptor
@@ -36,7 +37,8 @@ namespace VirtoCommerce.ImageToolsModule.Core
                     Name = "ImageTools.Thumbnails.ImageProcessJobCronExpression",
                     GroupName = "Thumbnail|General",
                     ValueType = SettingValueType.ShortText,
-                    DefaultValue = "0 0 * * *"
+                    DefaultValue = "0 0 * * *",
+                    RestartRequired = true
                 };
 
                 public static SettingDescriptor ProcessBatchSize = new SettingDescriptor
