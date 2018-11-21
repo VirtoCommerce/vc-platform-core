6f238952525b38b2ec1418e706f0b953eaa3995c
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-11-21 17:55:22 +0200

    Moved appDependencies

--- VirtoCommerce.Platform.Web/webpack.config.js
+++ VirtoCommerce.Platform.Web/webpack.config.js
@@ -1,14 +1,53 @@
 const path = require('path');
 const glob = require('glob');
+const webpack = require('webpack');
+const appDependencies = require('./wwwroot/js/app/appDependencies.json').appDependencies;
 
-module.exports = [{
+//console.log(glob.sync('./Modules/**/*.Web/Scripts/**/*.js', { ignore: ['./Modules/Module1/**/*.*', './**/allPackages.js'], nosort: true } ));
+
+module.exports = [
+    {
         entry: { 'main': './wwwroot/src/js/vendor.js' },
         output: {
             path: path.resolve(__dirname, 'wwwroot/dist'),
             filename: 'vendor.js',
             publicPath: 'dist/'
         },
-        mode: 'development'
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    loaders: ["style-loader", "css-loader"]
+                },
+                {
+                    test: /\.(jpe?g|png|gif)$/i,
+                    loader: "file-loader",
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets/images/'
+                        //the images will be emited to dist/assets/images/ folder
+                    }
+                },
+                {
+                    test: /\.modernizrrc\.js$/,
+                    loader: 'webpack-modernizr-loader'
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            })
+        ],
+        resolve: {
+            alias: {
+                Vendor: path.resolve(__dirname, 'wwwroot/vendor'),
+                modernizr$: path.resolve(__dirname, ".modernizrrc.js")
+            }
+        }
     },
     {
         entry: glob.sync('./wwwroot/js/**/*.js'),
@@ -17,5 +56,28 @@ module.exports = [{
             filename: 'app.js',
             publicPath: 'dist/'
         },
-        mode: 'development'
-}];
+        mode: 'production',
+        plugins: [
+            new webpack.ProvidePlugin({
+                _: 'underscore'
+            }),
+            new webpack.DefinePlugin({
+                'AppDependencies': JSON.stringify(appDependencies)
+            })
+        ]
+    },
+    {
+        entry: glob.sync('./Modules/**/*.Web/Scripts/**/*.js', { ignore: ['./Modules/Module1/**/*.*'], nosort: true }),
+        output: {
+            path: path.resolve(__dirname, 'wwwroot/dist'),
+            filename: 'modules.js',
+            publicPath: 'dist/'
+        },
+        mode: 'production',
+        plugins: [
+            new webpack.DefinePlugin({
+                'AppDependencies': JSON.stringify(appDependencies)
+            })
+        ]
+    }
+];
