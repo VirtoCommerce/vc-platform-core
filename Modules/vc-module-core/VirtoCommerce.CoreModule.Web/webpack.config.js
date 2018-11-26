a70401519b8a6ac53489beb99f7235f0019e8a7f
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-11-26 14:26:53 +0200

    Fixed search pattern

--- Modules/vc-module-core/VirtoCommerce.CoreModule.Web/webpack.config.js
+++ Modules/vc-module-core/VirtoCommerce.CoreModule.Web/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 
 module.exports = [
     {
-        entry: glob.sync('./Scripts/**/*.js', { ignore: ['./Scripts/dist/**/*.*', './Scripts/src/**/*.*'], nosort: true }),
+        entry: glob.sync('./Scripts/**/*.js', { ignore: ['./Scripts/dist/**/*.*'], nosort: true }),
         output: {
             path: path.resolve(__dirname, 'Scripts/dist'),
             filename: 'app.js'
