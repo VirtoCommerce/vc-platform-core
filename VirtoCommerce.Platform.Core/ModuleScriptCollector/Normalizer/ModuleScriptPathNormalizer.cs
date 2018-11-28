6370bf61124a4be53224e8f3384a8f3d2c977e5f
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-11-28 16:07:01 +0200

    Added test as demonstration how is working normalizer

--- VirtoCommerce.Platform.Core/ModuleScriptCollector/Normalizer/ModuleScriptPathNormalizer.cs
+++ VirtoCommerce.Platform.Core/ModuleScriptCollector/Normalizer/ModuleScriptPathNormalizer.cs
@@ -6,16 +6,14 @@ namespace VirtoCommerce.Platform.Core.ModuleScriptCollector.Normalizer
 {
     public class ModuleScriptPathNormalizer : IModuleScriptPathNormalizer
     {
-        private readonly LocalStorageModuleCatalogOptions _localStorageModuleCatalogOptions;
         private readonly string _rootPath;
         private readonly string _path;
         private readonly string _moduleName;
         private readonly string _moduleFolder;
 
-        public ModuleScriptPathNormalizer(LocalStorageModuleCatalogOptions localStorageMosduleCatalogOptions, string path, string moduleName, string moduleFolder)
+        public ModuleScriptPathNormalizer(LocalStorageModuleCatalogOptions localStorageModuleCatalogOptions, string path, string moduleName, string moduleFolder)
         {
-            _localStorageModuleCatalogOptions = localStorageMosduleCatalogOptions;
-            _rootPath = _localStorageModuleCatalogOptions.DiscoveryPath;
+            _rootPath = localStorageModuleCatalogOptions.DiscoveryPath;
             _path = path;
             _moduleName = moduleName;
             _moduleFolder = moduleFolder;
@@ -26,6 +24,7 @@ namespace VirtoCommerce.Platform.Core.ModuleScriptCollector.Normalizer
             return p;
         }
 
+        //TODO: Everything below should be rewritten
         string GetRelativePath(string fullPath, string moduleFolder)
         {
             return fullPath.Replace(_rootPath, "")
---
+++ VirtoCommerce.Platform.Tests/UnitTests/ModulePathNormalizerTest.cs
@@ -0,0 +1,33 @@
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.ModuleScriptCollector.Normalizer;
+using Xunit;
+
+namespace VirtoCommerce.Platform.Tests.UnitTests
+{
+    public class ModulePathNormalizerTest
+    {
+        [Fact]
+        public void TestNormalizer()
+        {
+            var rootPath = @"bla\bla\path";
+            var path = "bestModuleEver/VirtoCommerce.CatalogModule.Web/Scripts";
+            var moduleName = "targetName";
+            var moduleFolder = "SomeModuleFolder";
+            var fullFilePath = @"bla\bla\path\bestModuleEver\SomeModuleFolder\Scripts\dist\app.js";
+
+            var normalizer = new ModuleScriptPathNormalizer(GetLocalStorageModuleCatalogOptions(rootPath), path, moduleName, moduleFolder);
+
+            var result = normalizer.Normalize(fullFilePath);
+
+            Assert.Equal("/Modules/$(targetName)/Scripts/dist/app.js", result);
+        }
+
+        private static LocalStorageModuleCatalogOptions GetLocalStorageModuleCatalogOptions(string rootPath)
+        {
+            return new LocalStorageModuleCatalogOptions
+            {
+                DiscoveryPath = rootPath
+            };
+        }
+    }
+}
