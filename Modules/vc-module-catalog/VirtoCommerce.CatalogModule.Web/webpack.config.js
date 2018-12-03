035e132fa642bdb7913f36db20c9bb43fffec47f
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-03 15:59:49 +0200

    Added style loader

--- Modules/vc-module-catalog/VirtoCommerce.CatalogModule.Web/webpack.config.js
+++ Modules/vc-module-catalog/VirtoCommerce.CatalogModule.Web/webpack.config.js
@@ -1,12 +1,39 @@
 const glob = require("glob");
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const rootPath = path.resolve(__dirname, 'Scripts/dist');
+
+function getEntrypoints() {
+    const result = [
+        ...glob.sync('./Scripts/**/*.js', { ignore: ['./Scripts/dist/**/*.*'], nosort: true }),
+        ...glob.sync('./Content/**/*.css', { nosort: true })
+    ];
+
+    return result;
+}
 
 module.exports = [
     {
-        entry: glob.sync('./Scripts/**/*.js', { ignore: ['./Scripts/dist/**/*.*'], nosort: true }),
+        entry: getEntrypoints(),
         output: {
-            path: path.resolve(__dirname, 'Scripts/dist'),
+            path: rootPath,
             filename: 'app.js'
-        }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    loaders: [MiniCssExtractPlugin.loader, "css-loader"]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(rootPath, { verbose: true }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
+        ]
     }
 ];
