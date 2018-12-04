db6bdb14e76e72f2342917a8a640970424cb2739
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-04 13:01:55 +0200

    Fixed dist path

--- Modules/vc-module-subscriptions/VirtoCommerce.SubscriptionModule.Web/webpack.config.js
+++ Modules/vc-module-subscriptions/VirtoCommerce.SubscriptionModule.Web/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = [
     {
         entry: glob.sync('./Scripts/**/*.js', { ignore: ['./Scripts/dist/**/*.*'], nosort: true }),
         output: {
-            path: path.resolve(__dirname, 'Scripts/dist'),
+            path: path.resolve(__dirname, 'dist'),
             filename: 'app.js'
         }
     }
