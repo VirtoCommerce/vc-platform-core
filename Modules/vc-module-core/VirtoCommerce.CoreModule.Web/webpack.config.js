8d1f59240cae664dcb6da8f1ab496ad24c8aeb4e
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-04 12:54:04 +0200

    Changed dist path

--- Modules/vc-module-core/VirtoCommerce.CoreModule.Web/webpack.config.js
+++ Modules/vc-module-core/VirtoCommerce.CoreModule.Web/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const rootPath = path.resolve(__dirname, 'Scripts/dist');
+const rootPath = path.resolve(__dirname, 'dist');
 
 function getEntrypoints() {
     const result = [
