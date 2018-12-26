2613c58b576d3221facacfe49bd0c3e460716600
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-26 16:12:51 +0200

    Added checking user permissions before adding limited permission claim

--- VirtoCommerce.Platform.Web/Controllers/Api/AuthorizationController.cs
+++ VirtoCommerce.Platform.Web/Controllers/Api/AuthorizationController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,8 +16,6 @@ using OpenIddict.Core;
 using OpenIddict.EntityFrameworkCore.Models;
 using VirtoCommerce.Platform.Core;
 using VirtoCommerce.Platform.Core.Security;
-using VirtoCommerce.Platform.Security.Authorization;
-using VirtoCommerce.Platform.Web.Infrastructure;
 
 namespace Mvc.Server
 {
@@ -82,18 +81,29 @@ namespace Mvc.Server
 
                 // Create a new authentication ticket.
                 var ticket = await CreateTicketAsync(request, user);
-
-                // Set limited permissions and authenticate user with combined mode Cookies + Bearer.
-                //
-                // LimitedPermissions claims that will be granted to the user by cookies when bearer token authentication is enabled.
-                // This can help to authorize the user for direct(non - AJAX) GET requests to the VC platform API and / or to use some 3rd - party web applications for the VC platform(like Hangfire dashboard).
-                //
-                // If the user identity has claim named "limited_permissions", this attribute should authorize only permissions listed in that claim. Any permissions that are required by this attribute but
-                // not listed in the claim should cause this method to return false. However, if permission limits of user identity are not defined ("limited_permissions" claim is missing),
-                // then no limitations should be applied to the permissions.
                 var claims = await _userClaimsPrincipalFactory.CreateAsync(user);
-                ((ClaimsIdentity)claims.Identity).AddClaim(new Claim(PlatformConstants.Security.Claims.LimitedPermissionsClaimType, _authorizationOptions.LimitedCookiePermissions));
-                await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, claims);
+                var limitedPermissions = _authorizationOptions.LimitedCookiePermissions?.Split(';', StringSplitOptions.RemoveEmptyEntries) ?? new string[0];
+
+                var userHasAnyPermissions = true;
+
+                if (!user.Roles.Select(r => r.Name).Contains(PlatformConstants.Security.Roles.Administrator))
+                {
+                    userHasAnyPermissions = claims.Claims.Where(c => c.Type == "permission").Select(c => c.Value).Intersect(limitedPermissions, StringComparer.OrdinalIgnoreCase).Any();
+                }
+
+                if (userHasAnyPermissions)
+                {
+                    // Set limited permissions and authenticate user with combined mode Cookies + Bearer.
+                    //
+                    // LimitedPermissions claims that will be granted to the user by cookies when bearer token authentication is enabled.
+                    // This can help to authorize the user for direct(non - AJAX) GET requests to the VC platform API and / or to use some 3rd - party web applications for the VC platform(like Hangfire dashboard).
+                    //
+                    // If the user identity has claim named "limited_permissions", this attribute should authorize only permissions listed in that claim. Any permissions that are required by this attribute but
+                    // not listed in the claim should cause this method to return false. However, if permission limits of user identity are not defined ("limited_permissions" claim is missing),
+                    // then no limitations should be applied to the permissions.
+                    ((ClaimsIdentity)claims.Identity).AddClaim(new Claim(PlatformConstants.Security.Claims.LimitedPermissionsClaimType, _authorizationOptions.LimitedCookiePermissions));
+                    await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, claims);
+                }
 
                 return SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
             }
