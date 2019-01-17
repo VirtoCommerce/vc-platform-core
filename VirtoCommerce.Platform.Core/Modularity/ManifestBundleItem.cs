ef3c4c0344d8c1176282717760ccc60a42e79298
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2019-01-17 09:52:28 +0200

    Fix manifests

--- VirtoCommerce.Platform.Core/Modularity/ManifestBundleItem.cs
+++ VirtoCommerce.Platform.Core/Modularity/ManifestBundleItem.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace VirtoCommerce.Platform.Core.Modularity
 {
-	public class ManifestBundleItem
-	{
-		[XmlAttribute("virtualPath")]
-		public string VirtualPath { get; set; }
-	}
+    public class ManifestBundleItem
+    {
+        [XmlElement(ElementName = "file")]
+        public List<string> FileName { get; set; }
+
+        [XmlAttribute(AttributeName = "virtualPath")]
+        public string VirtualPath { get; set; }
+    }
 }
