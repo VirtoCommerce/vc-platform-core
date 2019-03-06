b3e29397ea10a66cf959f1bd85bcbf83d2a98dfe
anivirtoway <ani@virtoway.com>
2019-03-06 12:35:51 +0300

    toggle notification

--- VirtoCommerce.Platform.Web/wwwroot/js/app/pushNotifications/headerNotificationWidget.js
+++ VirtoCommerce.Platform.Web/wwwroot/js/app/pushNotifications/headerNotificationWidget.js
@@ -1,45 +1,42 @@
-angular.module('platformWebApp')
-  .directive('vaHeaderNotificationWidget', ["$document",
-    function ($document) {
+angular.module('platformWebApp').directive('vaHeaderNotificationWidget', ["$document",
+  function ($document) {
 
-      return {
-        restrict: 'E',
-        replace: true,
-        transclude: true,
-        templateUrl: '$(Platform)/Scripts/app/pushNotifications/headerNotificationWidget.tpl.html',
-        link: function (scope, element, attr, ngModelController, linker) {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      templateUrl: '$(Platform)/Scripts/app/pushNotifications/headerNotificationWidget.tpl.html',
+      link: function (scope, element, attr, ngModelController, linker) {
 
-          scope.noticeOpened = false;
+        scope.noticeOpened = false;
 
-          function handleClickEvent(event) {
-            var dropdownElement = $document.find('.notification-widget .header__nav-item');
-            var hadDropdownElement = $document.find('.notification-widget .header__nav-item .header__nav-item--opened');
-            if (scope.noticeOpened && !(dropdownElement.is(event.target) || dropdownElement.has(event.target).length > 0 ||
-              hadDropdownElement.is(event.target) || hadDropdownElement.has(event.target).length > 0)) {
-              scope.$apply(function () {
-                scope.noticeOpened = false;
-              });
-            }
-            console.log(scope.noticeOpened);
+        function handleClickEvent(event) {
+          console.log(scope.noticeOpened);
+          var dropdownElement = $document.find('[notificationWidget]');
+          var hadDropdownElement = $document.find('[notificationWidget] .header__nav-item--opened');
+          if (scope.noticeOpened && !(dropdownElement.is(event.target) || dropdownElement.has(event.target).length > 0 ||
+            hadDropdownElement.is(event.target) || hadDropdownElement.has(event.target).length > 0)) {
+            scope.$apply(function () {
+              scope.noticeOpened = false;
+            });
           }
-
-          $document.bind('click', handleClickEvent);
-
-          scope.$on('$destroy', function () {
-            $document.unbind('click', handleClickEvent);
-          });
-
-          scope.toggleNotice = function () {
-            var widgetIcon = $document.find('.notification-widget .header__nav-item');
-
-            scope.noticeOpened = !scope.noticeOpened;
-          };
-
-          scope.close = function () {
-            debugger;
-            scope.noticeOpened = false;
-          };
-
+          console.log(scope.noticeOpened);
         }
+
+        $document.bind('click', handleClickEvent);
+
+        scope.$on('$destroy', function () {
+          $document.unbind('click', handleClickEvent);
+        });
+
+        scope.toggleNotice = function () {
+          scope.noticeOpened = !scope.noticeOpened;
+        };
+
+        scope.close = function () {
+          scope.noticeOpened = false;
+        };
+
       }
-    }]);
+    }
+  }]);
