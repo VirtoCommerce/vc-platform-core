1a32eaf9fd03c914fcbfcac4a25ccfd780295278
anivirtoway <ani@virtoway.com>
2019-03-18 09:23:14 +0300

    add header tooltips, remove menu license info, purchase and renew button open license blade, header license widget show license info and aligned to blade width, add userpic.svg

--- VirtoCommerce.Platform.Web/wwwroot/js/app/pushNotifications/headerNotificationWidget.js
+++ VirtoCommerce.Platform.Web/wwwroot/js/app/pushNotifications/headerNotificationWidget.js
@@ -1,110 +1,111 @@
 angular.module('platformWebApp')
-  .factory('platformWebApp.headerNotificationWidgetService', ['platformWebApp.pushNotifications',
-    function (notifications) {
+    .factory('platformWebApp.headerNotificationWidgetService', ['platformWebApp.pushNotifications',
+        function (notifications) {
 
-      var notifications = [];
-      var newCount = 0;
+            var notifications = [];
+            var newCount = 0;
 
-      function addNotification(notification) {
-        var exitstItem = _.find(notifications, function (x) { return x.id == notification.id; });
-        if (exitstItem) {
-          angular.copy(notification, exitstItem);
-        } else {
-          notifications.push(notification);
-          newCount = newCount + 1;
-        }
-      }
-
-      function clear() {
-        notifications = [];
-        newCount = 0;
-      }
-
-      function getNotifications() {
-        return notifications;
-      }
-
-      function getNewCount() {
-        return newCount;
-      }
-
-      function markAllAsReaded() {
-        newCount = 0;
-      }
-
-      function newAviable() {
-        return newCount > 0;
-      }
-
-      var retVal = {
-        addNotification: addNotification,
-        getNotifications: getNotifications,
-        getNewCount: getNewCount,
-        newAviable: newAviable,
-        markAllAsReaded: markAllAsReaded,
-        clear: clear
-      };
-
-      return retVal;
-    }])
-  .directive('vaHeaderNotificationWidget', ['$document', 'platformWebApp.headerNotificationWidgetService', '$state',
-    function ($document, headerNotifications, $state) {
-
-      return {
-        restrict: 'E',
-        replace: true,
-        transclude: true,
-        scope: {},
-        templateUrl: '$(Platform)/Scripts/app/pushNotifications/headerNotificationWidget.tpl.html',
-        link: function (scope) {
-
-          scope.dropDownOpened = false;
-          scope.getNotifications = headerNotifications.getNotifications;
-          scope.getNewCount = headerNotifications.getNewCount;
-          scope.newAviable = headerNotifications.newAviable;
-
-          function handleClickEvent(event) {
-            var dropdownElement = $document.find('[notificationWidget]');
-            var hadDropdownElement = $document.find('[notificationWidget] .header__nav-item--opened');
-            if (scope.dropDownOpened && !(dropdownElement.is(event.target) || dropdownElement.has(event.target).length ||
-              hadDropdownElement.is(event.target) || hadDropdownElement.has(event.target).length)) {
-              scope.$apply(function () {
-                scope.dropDownOpened = false;
-              });
+            function addNotification(notification) {
+                var exitstItem = _.find(notifications, function (x) { return x.id == notification.id; });
+                if (exitstItem) {
+                    angular.copy(notification, exitstItem);
+                } else {
+                    notifications.push(notification);
+                    newCount = newCount + 1;
+                }
             }
-          }
 
-          $document.bind('click', handleClickEvent);
-
-          scope.$on('$destroy', function () {
-            $document.unbind('click', handleClickEvent);
-          });
-
-          scope.toggleDropDown = function () {
-            scope.dropDownOpened = !scope.dropDownOpened;
-            if (scope.dropDownOpened) {
-              headerNotifications.markAllAsReaded();
+            function clear() {
+                notifications = [];
+                newCount = 0;
             }
-          };
 
-          scope.close = function () {
-            scope.dropDownOpened = false;
-          };
+            function getNotifications() {
+                return notifications;
+            }
 
-          scope.clearRecent = function () {
-            headerNotifications.clear();
-          }
+            function getNewCount() {
+                return newCount;
+            }
 
-          scope.viewHistory = function () {
-            scope.dropDownOpened = false;
-            $state.go('workspace.pushNotificationsHistory');
-          }
+            function markAllAsReaded() {
+                newCount = 0;
+            }
 
-          scope.notificationClick = function (notification) {
-            scope.dropDownOpened = false;
-            notification.action(notification);
-          }
+            function newAviable() {
+                return newCount > 0;
+            }
 
-        }
-      }
-    }]);
+            var retVal = {
+                addNotification: addNotification,
+                getNotifications: getNotifications,
+                getNewCount: getNewCount,
+                newAviable: newAviable,
+                markAllAsReaded: markAllAsReaded,
+                clear: clear
+            };
+
+            return retVal;
+        }])
+    .directive('vaHeaderNotificationWidget', ['$document', 'platformWebApp.headerNotificationWidgetService', '$state',
+        function ($document, headerNotifications, $state) {
+
+            return {
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                scope: {},
+                templateUrl: '$(Platform)/Scripts/app/pushNotifications/headerNotificationWidget.tpl.html',
+                link: function (scope) {
+
+                    scope.dropDownOpened = false;
+                    scope.getNotifications = headerNotifications.getNotifications;
+                    scope.getNewCount = headerNotifications.getNewCount;
+                    scope.newAviable = headerNotifications.newAviable;
+                    console.log(scope);
+
+                    function handleClickEvent(event) {
+                        var dropdownElement = $document.find('[notificationWidget]');
+                        var hadDropdownElement = $document.find('[notificationWidget] .header__nav-item--opened');
+                        if (scope.dropDownOpened && !(dropdownElement.is(event.target) || dropdownElement.has(event.target).length ||
+                            hadDropdownElement.is(event.target) || hadDropdownElement.has(event.target).length)) {
+                            scope.$apply(function () {
+                                scope.dropDownOpened = false;
+                            });
+                        }
+                    }
+
+                    $document.bind('click', handleClickEvent);
+
+                    scope.$on('$destroy', function () {
+                        $document.unbind('click', handleClickEvent);
+                    });
+
+                    scope.toggleDropDown = function () {
+                        scope.dropDownOpened = !scope.dropDownOpened;
+                        if (scope.dropDownOpened) {
+                            headerNotifications.markAllAsReaded();
+                        }
+                    };
+
+                    scope.close = function () {
+                        scope.dropDownOpened = false;
+                    };
+
+                    scope.clearRecent = function () {
+                        headerNotifications.clear();
+                    }
+
+                    scope.viewHistory = function () {
+                        scope.dropDownOpened = false;
+                        $state.go('workspace.pushNotificationsHistory');
+                    }
+
+                    scope.notificationClick = function (notification) {
+                        scope.dropDownOpened = false;
+                        notification.action(notification);
+                    }
+
+                }
+            }
+        }]);
--- VirtoCommerce.Platform.Web/wwwroot/js/app/pushNotifications/pushNotifications.js
+++ VirtoCommerce.Platform.Web/wwwroot/js/app/pushNotifications/pushNotifications.js
@@ -1,97 +1,97 @@
 import * as signalR from '@aspnet/signalr';
 
 angular.module('platformWebApp').config(
-  ['$stateProvider', function ($stateProvider) {
-    $stateProvider
-      .state('workspace.pushNotificationsHistory', {
-        url: '/events',
-        templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-        controller: ['$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
-          var blade = {
-            id: 'events',
-            title: 'platform.blades.history.title',
-            breadcrumbs: [],
-            subtitle: 'platform.blades.history.subtitle',
-            controller: 'platformWebApp.pushNotificationsHistoryController',
-            template: '$(Platform)/Scripts/app/pushNotifications/blade/history.tpl.html',
-            isClosingDisabled: true
-          };
-          bladeNavigationService.showBlade(blade);
-        }
-        ]
-      });
-  }])
-  .factory('platformWebApp.pushNotificationTemplateResolver', ['platformWebApp.bladeNavigationService', '$state', function (bladeNavigationService, $state) {
-    var notificationTemplates = [];
+    ['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('workspace.pushNotificationsHistory', {
+                url: '/events',
+                templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
+                controller: ['$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
+                    var blade = {
+                        id: 'events',
+                        title: 'platform.blades.history.title',
+                        breadcrumbs: [],
+                        subtitle: 'platform.blades.history.subtitle',
+                        controller: 'platformWebApp.pushNotificationsHistoryController',
+                        template: '$(Platform)/Scripts/app/pushNotifications/blade/history.tpl.html',
+                        isClosingDisabled: true
+                    };
+                    bladeNavigationService.showBlade(blade);
+                }
+                ]
+            });
+    }])
+    .factory('platformWebApp.pushNotificationTemplateResolver', ['platformWebApp.bladeNavigationService', '$state', function (bladeNavigationService, $state) {
+        var notificationTemplates = [];
 
-    function register(template) {
-      notificationTemplates.push(template);
-      notificationTemplates.sort(function (a, b) { return a.priority - b.priority; });
-    };
-    function resolve(notification, place) {
-      return _.find(notificationTemplates, function (x) { return x.satisfy(notification, place); });
-    };
-    var retVal = {
-      register: register,
-      resolve: resolve,
-    };
-
-    //Recent events notification template (error, info, debug)
-    var headerNotificationDefaultTemplate =
-    {
-      priority: 1000,
-      satisfy: function (notification, place) { return place == 'header-notification'; },
-      //template for display that notification in menu and list
-      template: '$(Platform)/Scripts/app/pushNotifications/headerNotificationDefault.tpl.html',
-      //action executed when notification selected
-      action: function (notify) { $state.go('workspace.pushNotificationsHistory', notify) }
-    };
-
-    //In history list notification template (error, info, debug)
-    var historyDefaultTemplate =
-    {
-      priority: 1000,
-      satisfy: function (notification, place) { return place == 'history'; },
-      //template for display that notification in menu and list
-      template: '$(Platform)/Scripts/app/pushNotifications/blade/historyDefault.tpl.html',
-      //action executed in event detail
-      action: function (notify) {
-        var blade = {
-          id: 'notifyDetail',
-          title: 'platform.blades.historyDetailDefault.title',
-          subtitle: 'platform.blades.historyDetailDefault.subtitle',
-          template: '$(Platform)/Scripts/app/pushNotifications/blade/historyDetailDefault.tpl.html',
-          isClosingDisabled: false,
-          notify: notify
+        function register(template) {
+            notificationTemplates.push(template);
+            notificationTemplates.sort(function (a, b) { return a.priority - b.priority; });
         };
-        bladeNavigationService.showBlade(blade);
-      }
-    };
+        function resolve(notification, place) {
+            return _.find(notificationTemplates, function (x) { return x.satisfy(notification, place); });
+        };
+        var retVal = {
+            register: register,
+            resolve: resolve,
+        };
 
-    retVal.register(headerNotificationDefaultTemplate);
-    retVal.register(historyDefaultTemplate);
+        //Recent events notification template (error, info, debug)
+        var headerNotificationDefaultTemplate =
+        {
+            priority: 1000,
+            satisfy: function (notification, place) { return place == 'header-notification'; },
+            //template for display that notification in menu and list
+            template: '$(Platform)/Scripts/app/pushNotifications/headerNotificationDefault.tpl.html',
+            //action executed when notification selected
+            action: function (notify) { $state.go('workspace.pushNotificationsHistory', notify) }
+        };
 
-    return retVal;
-  }])
-  .factory('platformWebApp.pushNotificationService', ['$rootScope', 'platformWebApp.pushNotificationTemplateResolver', 'platformWebApp.headerNotificationWidgetService',
-    function ($rootScope, eventTemplateResolver, headerNotifications) {
+        //In history list notification template (error, info, debug)
+        var historyDefaultTemplate =
+        {
+            priority: 1000,
+            satisfy: function (notification, place) { return place == 'history'; },
+            //template for display that notification in menu and list
+            template: '$(Platform)/Scripts/app/pushNotifications/blade/historyDefault.tpl.html',
+            //action executed in event detail
+            action: function (notify) {
+                var blade = {
+                    id: 'notifyDetail',
+                    title: 'platform.blades.historyDetailDefault.title',
+                    subtitle: 'platform.blades.historyDetailDefault.subtitle',
+                    template: '$(Platform)/Scripts/app/pushNotifications/blade/historyDetailDefault.tpl.html',
+                    isClosingDisabled: false,
+                    notify: notify
+                };
+                bladeNavigationService.showBlade(blade);
+            }
+        };
 
-      //SignalR setup connection
-      var connection = new signalR.HubConnectionBuilder()
-        .withUrl("/pushNotificationHub")
-        .build();
-      connection.start();
+        retVal.register(headerNotificationDefaultTemplate);
+        retVal.register(historyDefaultTemplate);
 
-      connection.on('Send', function (data) {
-        $rootScope.$broadcast("new-notification-event", data);
+        return retVal;
+    }])
+    .factory('platformWebApp.pushNotificationService', ['$rootScope', 'platformWebApp.pushNotificationTemplateResolver', 'platformWebApp.headerNotificationWidgetService',
+        function ($rootScope, eventTemplateResolver, headerNotifications) {
 
-        var notificationTemplate = eventTemplateResolver.resolve(data, 'header-notification');
-        data.template = notificationTemplate.template;
-        data.action = notificationTemplate.action;
-        headerNotifications.addNotification(data);
-      });
+            //SignalR setup connection
+            var connection = new signalR.HubConnectionBuilder()
+                .withUrl("/pushNotificationHub")
+                .build();
+            connection.start();
 
-      var retVal = {};
-      return retVal;
+            connection.on('Send', function (data) {
+                $rootScope.$broadcast("new-notification-event", data);
 
-    }]);
+                var notificationTemplate = eventTemplateResolver.resolve(data, 'header-notification');
+                data.template = notificationTemplate.template;
+                data.action = notificationTemplate.action;
+                headerNotifications.addNotification(data);
+            });
+
+            var retVal = {};
+            return retVal;
+
+        }]);
