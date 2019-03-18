f5d07b7465a3295de2a66a83ea38fe0cee2fa435
Andrei Kostyrin <kostyrin2@gmail.com>
2019-03-18 15:56:21 +0200

    #72 ImportImages

--- Modules/vc-module-catalog/VirtoCommerce.CatalogModule.Data/ExportImport/CatalogExportImport.cs
+++ Modules/vc-module-catalog/VirtoCommerce.CatalogModule.Data/ExportImport/CatalogExportImport.cs
@@ -9,6 +9,7 @@ using VirtoCommerce.CatalogModule.Core.Model;
 using VirtoCommerce.CatalogModule.Core.Model.Search;
 using VirtoCommerce.CatalogModule.Core.Search;
 using VirtoCommerce.CatalogModule.Core.Services;
+using VirtoCommerce.Platform.Core.Assets;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.ExportImport;
 using VirtoCommerce.Platform.Data.ExportImport;
@@ -27,10 +28,12 @@ namespace VirtoCommerce.CatalogModule.Data.ExportImport
         private readonly IProperyDictionaryItemSearchService _propertyDictionarySearchService;
         private readonly IProperyDictionaryItemService _propertyDictionaryService;
         private readonly JsonSerializer _jsonSerializer;
+        private readonly IBlobStorageProvider _blobStorageProvider;
 
         private int _batchSize = 50;
 
-        public CatalogExportImport(ICatalogService catalogService, ICatalogSearchService catalogSearchService, ICategoryService categoryService, IItemService itemService, IPropertyService propertyService, IProperyDictionaryItemSearchService propertyDictionarySearchService, IProperyDictionaryItemService propertyDictionaryService, IProductSearchService productSearchService, ICategorySearchService categorySearchService, IOptions<MvcJsonOptions> jsonOptions)
+        public CatalogExportImport(ICatalogService catalogService, ICatalogSearchService catalogSearchService, ICategoryService categoryService, IItemService itemService, IPropertyService propertyService, IProperyDictionaryItemSearchService propertyDictionarySearchService, IProperyDictionaryItemService propertyDictionaryService, IProductSearchService productSearchService, ICategorySearchService categorySearchService, IOptions<MvcJsonOptions> jsonOptions
+            , IBlobStorageProvider blobStorageProvider)
         {
             _catalogService = catalogService;
             _catalogSearchService = catalogSearchService;
@@ -41,7 +44,8 @@ namespace VirtoCommerce.CatalogModule.Data.ExportImport
             _propertyDictionaryService = propertyDictionaryService;
             _productSearchService = productSearchService;
             _categorySearchService = categorySearchService;
-            _jsonSerializer = JsonSerializer.Create(jsonOptions.Value.SerializerSettings); ;
+            _jsonSerializer = JsonSerializer.Create(jsonOptions.Value.SerializerSettings);
+            _blobStorageProvider = blobStorageProvider;
         }
 
         public async Task DoExportAsync(Stream outStream, ExportImportOptions options, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
@@ -77,6 +81,11 @@ namespace VirtoCommerce.CatalogModule.Data.ExportImport
                 {
                     var searchResult = await _catalogSearchService.SearchAsync(new CatalogListEntrySearchCriteria { Skip = skip, Take = take, ResponseGroup = SearchResponseGroup.WithCategories });
                     var categories = await _categoryService.GetByIdsAsync(searchResult.Categories.Select(x => x.Id).ToArray(), CategoryResponseGroup.Full);
+                    if (options.HandleBinaryData)
+                    {
+                        LoadImages(categories, progressInfo);
+                    }
+
                     return new GenericSearchResult<Category> { Results = categories, TotalCount = searchResult.Categories.Count };
                 }, (processedCount, totalCount) =>
                 {
@@ -124,6 +133,11 @@ namespace VirtoCommerce.CatalogModule.Data.ExportImport
                 {
                     var searchResult = await _catalogSearchService.SearchAsync(new CatalogListEntrySearchCriteria { Skip = skip, Take = take, ResponseGroup = SearchResponseGroup.WithProducts });
                     var products = await _itemService.GetByIdsAsync(searchResult.Products.Select(x => x.Id).ToArray(), ItemResponseGroup.ItemLarge);
+                    if (options.HandleBinaryData)
+                    {
+                        LoadImages(products, progressInfo);
+                    }
+
                     return new GenericSearchResult<CatalogProduct> { Results = products, TotalCount = searchResult.ProductsTotalCount };
                 }, (processedCount, totalCount) =>
                 {
@@ -161,7 +175,15 @@ namespace VirtoCommerce.CatalogModule.Data.ExportImport
                         }
                         else if (reader.Value.ToString() == "Categories")
                         {
-                            await reader.DeserializeJsonArrayWithPagingAsync<Category>(_jsonSerializer, _batchSize, items => _categoryService.SaveChangesAsync(items.ToArray()), processedCount =>
+                            await reader.DeserializeJsonArrayWithPagingAsync<Category>(_jsonSerializer, _batchSize, async (items) => {
+                                var itemsArray = items.ToArray();
+                                await _categoryService.SaveChangesAsync(itemsArray);
+                                if (options.HandleBinaryData)
+                                {
+                                    ImportImages(itemsArray, progressInfo);
+                                }
+
+                            }, processedCount =>
                             {
                                 progressInfo.Description = $"{ processedCount } categories have been imported";
                                 progressCallback(progressInfo);
@@ -185,13 +207,66 @@ namespace VirtoCommerce.CatalogModule.Data.ExportImport
                         }
                         else if (reader.Value.ToString() == "Products")
                         {
-                            await reader.DeserializeJsonArrayWithPagingAsync<CatalogProduct>(_jsonSerializer, _batchSize, items => _itemService.SaveChangesAsync(items.ToArray()), processedCount =>
+                            await reader.DeserializeJsonArrayWithPagingAsync<CatalogProduct>(_jsonSerializer, _batchSize, async (items) => {
+                                var itemsArray = items.ToArray();
+                                await _itemService.SaveChangesAsync(itemsArray);
+                                if (options.HandleBinaryData)
+                                {
+                                    ImportImages(itemsArray, progressInfo);
+                                }
+                            }, processedCount =>
                             {
                                 progressInfo.Description = $"{ processedCount } products have been imported";
                                 progressCallback(progressInfo);
                             }, cancellationToken);
                         }
                     }
+                }
+            }
+        }
+
+        private void LoadImages(IHasImages[] haveImagesObjects, ExportImportProgressInfo progressInfo)
+        {
+            var allImages = haveImagesObjects.SelectMany(x => x.GetFlatObjectsListWithInterface<IHasImages>())
+                                             .SelectMany(x => x.Images).ToArray();
+            foreach (var image in allImages)
+            {
+                try
+                {
+                    using (var stream = _blobStorageProvider.OpenRead(image.Url))
+                    {
+                        image.BinaryData = stream.ReadFully();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    progressInfo.Errors.Add(ex.Message);
+                }
+            }
+        }
+
+        private void ImportImages(IHasImages[] haveImagesObjects, ExportImportProgressInfo progressInfo)
+        {
+
+            var allImages = haveImagesObjects.SelectMany(x => x.GetFlatObjectsListWithInterface<IHasImages>())
+                                       .SelectMany(x => x.Images).ToArray();
+            foreach (var image in allImages.Where(x => x.BinaryData != null))
+            {
+                try
+                {
+                    //do not save images with external url
+                    if (image.Url != null && !image.Url.IsAbsoluteUrl())
+                    {
+                        using (var sourceStream = new MemoryStream(image.BinaryData))
+                        using (var targetStream = _blobStorageProvider.OpenWrite(image.Url))
+                        {
+                            sourceStream.CopyTo(targetStream);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    progressInfo.Errors.Add(ex.Message);
                 }
             }
         }
