5aac7c355f95ff9b7b8329f014f3ebca59ec91cb
Eugeny Tatarincev <et@virtoway.com>
2019-04-01 16:06:45 +0200

    Fix import for catalog

---
+++ Modules/vc-module-catalog/VirtoCommerce.CatalogModule.Data/Migrations/20190401133841_CascadeDeleteCategoryRelations.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.CatalogModule.Data.Migrations
+{
+    public partial class CascadeDeleteCategoryRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryItemRelation_Item_ItemId",
+                table: "CategoryItemRelation");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryRelation_Category_SourceCategoryId",
+                table: "CategoryRelation");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryItemRelation_Item_ItemId",
+                table: "CategoryItemRelation",
+                column: "ItemId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryRelation_Category_SourceCategoryId",
+                table: "CategoryRelation",
+                column: "SourceCategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryItemRelation_Item_ItemId",
+                table: "CategoryItemRelation");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryRelation_Category_SourceCategoryId",
+                table: "CategoryRelation");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryItemRelation_Item_ItemId",
+                table: "CategoryItemRelation",
+                column: "ItemId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryRelation_Category_SourceCategoryId",
+                table: "CategoryRelation",
+                column: "SourceCategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
