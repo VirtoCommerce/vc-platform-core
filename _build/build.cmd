f8bd57f432b9e1efa5cf649063725e4ccb418c0e
Fazylzyanov Arsen <arsen.fazylzyanov@okiroya.ru>
2019-04-26 11:51:01 +0500

    Added to build cmd another run scenario

--- _build/build.cmd
+++ _build/build.cmd
@@ -1,11 +1,28 @@
+setlocal enabledelayedexpansion
+
 SET PROJECT_PATH=%~dp0%
 SET ARTIFACTS_PATH=%1
+
+IF NOT DEFINED ARTIFACTS_PATH (
+  SET ARTIFACTS_PATH=%~dp0%
+)
 
 cd ..
 cd /d VirtoCommerce.Platform.Web
 
 call npm i
+IF !ERRORLEVEL! NEQ 0 goto error
 
 call npm run webpack:build
+IF !ERRORLEVEL! NEQ 0 goto error
 
 call dotnet publish -c Release -o %ARTIFACTS_PATH% -f netcoreapp2.2 --self-contained false
+IF !ERRORLEVEL! NEQ 0 goto error
+
+goto end
+
+:error
+echo An error has occurred during platform build.
+
+:end
+echo Finished successfully.
