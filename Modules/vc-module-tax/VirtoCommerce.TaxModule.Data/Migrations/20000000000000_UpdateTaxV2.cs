3d773695e9304d47e93bfe0b0f9c16466d715706
Eugeny Tatarincev <et@virtoway.com>
2019-05-09 10:10:45 +0200

    Minor backward compatibility fixes

--- Modules/vc-module-tax/VirtoCommerce.TaxModule.Data/Migrations/20000000000000_UpdateTaxV2.cs
+++ Modules/vc-module-tax/VirtoCommerce.TaxModule.Data/Migrations/20000000000000_UpdateTaxV2.cs
@@ -12,6 +12,9 @@ namespace VirtoCommerce.TaxModule.Data.Migrations
                     BEGIN
 	                    INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId],[ProductVersion]) VALUES ('20190409093711_InitialTax', '2.2.3-servicing-35854')
                         ALTER TABLE [StoreTaxProvider] ADD [TypeName] nvarchar(128) NOT NULL Default ('')
+                    END
+                    BEGIN
+                        UPDATE StoreTaxProvider SET [TypeName] = 'FixedTaxRateProvider' WHERE [Code] = 'FixedRate'
                     END");
         }
 
