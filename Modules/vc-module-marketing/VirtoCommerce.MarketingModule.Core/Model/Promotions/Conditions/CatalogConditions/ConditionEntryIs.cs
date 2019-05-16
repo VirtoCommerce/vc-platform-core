38771bd3ad1fa27511d728d92f7a44cf1694f460
Aleksandr Andreev <alivemenone@gmail.com>
2019-05-16 12:01:42 +0300

    Catalog conditions blade with support multiple selection products. (#57)

--- Modules/vc-module-marketing/VirtoCommerce.MarketingModule.Core/Model/Promotions/Conditions/CatalogConditions/ConditionEntryIs.cs
+++ Modules/vc-module-marketing/VirtoCommerce.MarketingModule.Core/Model/Promotions/Conditions/CatalogConditions/ConditionEntryIs.cs
@@ -1,3 +1,4 @@
+using System;
 using VirtoCommerce.CoreModule.Core.Common;
 using VirtoCommerce.CoreModule.Core.Conditions;
 
@@ -9,16 +10,32 @@ namespace VirtoCommerce.MarketingModule.Core.Model.Promotions.Conditions
 
         public string ProductId { get; set; }
         public string ProductName { get; set; }
+        public string[] ProductIds { get; set; }
+        public string[] ProductNames { get; set; }
 
         /// <summary>
         /// ((PromotionEvaluationContext)x).IsItemInProduct(ProductId)
         /// </summary>
         public override bool Evaluate(IEvaluationContext context)
         {
+
+            if (ProductId == null && ProductIds == null)
+            {
+                throw new ArgumentException($"{nameof(ProductId)} and {nameof(ProductIds)} cannot be null.");
+            }
+
             var result = false;
             if (context is PromotionEvaluationContext promotionEvaluationContext)
             {
-                result = promotionEvaluationContext.IsItemInProduct(ProductId);
+                if (ProductIds != null)
+                {
+                    result = promotionEvaluationContext.IsItemInProduct(ProductId);
+                }
+                else
+                {
+                    result = promotionEvaluationContext.IsItemInProducts(ProductIds);
+                }
+
             }
 
             return result;
