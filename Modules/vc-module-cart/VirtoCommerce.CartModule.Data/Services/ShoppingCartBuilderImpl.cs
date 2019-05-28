0b1be4512a9ba1badcc2e5b47ffe5df2050cd9d8
anivirtoway <ani@virtoway.com>
2019-05-28 16:31:31 +0300

    fix ShoppingCartBuilder to use ShippingRateEvaluationContext instead of ShippingEvaluationContext

--- Modules/vc-module-cart/VirtoCommerce.CartModule.Data/Services/ShoppingCartBuilderImpl.cs
+++ Modules/vc-module-cart/VirtoCommerce.CartModule.Data/Services/ShoppingCartBuilderImpl.cs
@@ -260,7 +260,10 @@ namespace VirtoCommerce.CartModule.Data.Services
         public virtual async Task<ICollection<ShippingRate>> GetAvailableShippingRatesAsync()
         {
             // TODO: Remake with shipmentId
-            var shippingEvaluationContext = new ShippingEvaluationContext(Cart);
+            var shippingEvaluationContext = new ShippingRateEvaluationContext
+            {
+                Currency = Cart.Currency
+            };
 
             var criteria = new ShippingMethodsSearchCriteria
             {
