8044370a86626a62800a025ffd965e198ca5bd0e
Andrey Nikutin <45942974+anivirtoway@users.noreply.github.com>
2019-06-03 16:06:54 +0300

    fix sorting settings list, fix settings localization, fix russian languages (#311)

--- VirtoCommerce.Platform.Web/wwwroot/js/app/settings/blades/settingGroup-list.js
+++ VirtoCommerce.Platform.Web/wwwroot/js/app/settings/blades/settingGroup-list.js
@@ -1,6 +1,5 @@
-angular.module('platformWebApp')
-.controller('platformWebApp.settingGroupListController', ['$window', 'platformWebApp.modules', '$scope', 'platformWebApp.settings', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService',
-function ($window, modules, $scope, settings, bladeNavigationService, dialogService) {
+angular.module('platformWebApp').controller('platformWebApp.settingGroupListController', ['$window', 'platformWebApp.modules', '$scope', 'platformWebApp.settings', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService',
+    function ($window, modules, $scope, settings, bladeNavigationService, dialogService) {
     var settingsTree;
     var blade = $scope.blade;
 
@@ -8,8 +7,10 @@ function ($window, modules, $scope, settings, bladeNavigationService, dialogServ
         blade.isLoading = true;
 
         settings.query({}, function (results) {
+            results = _.sortBy(results, 'groupName');
             blade.allSettings = results;
             settingsTree = {};
+
             _.each(results, function (setting) {
                 var paths = (setting.groupName ? setting.groupName : 'General').split('|');
                 var lastParent = settingsTree;
@@ -52,7 +53,7 @@ function ($window, modules, $scope, settings, bladeNavigationService, dialogServ
                 $scope.selectNode({ groupName: $scope.selectedNodeId }, disableOpenAnimation);
             }
         },
-        function (error) { bladeNavigationService.setError('Error ' + error.status, blade); });
+            function (error) { bladeNavigationService.setError('Error ' + error.status, blade); });
     };
 
     $scope.selectNode = function (node, disableOpenAnimation) {
@@ -122,12 +123,12 @@ function ($window, modules, $scope, settings, bladeNavigationService, dialogServ
     });
 
     blade.toolbarCommands = [
-          {
-              name: "platform.commands.restart", icon: 'fa fa-bolt',
-              executeMethod: function () { restart(); },
-              canExecuteMethod: function () { return !blade.isLoading; },
-              permission: 'platform:module:manage'
-          }
+        {
+            name: "platform.commands.restart", icon: 'fa fa-bolt',
+            executeMethod: function () { restart(); },
+            canExecuteMethod: function () { return !blade.isLoading; },
+            permission: 'platform:module:manage'
+        }
     ];
 
     function restart() {
