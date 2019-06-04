c575d50b7a922b3eb7d275fdf6967ab1880b62b2
Andrey Nikutin <ani@virtoway.com>
2019-06-04 12:15:29 +0300

    rename store and content initial migration

--- Modules/vc-module-store/VirtoCommerce.StoreModule.Data/Migrations/20190522054351_InitialStore.cs
+++ Modules/vc-module-store/VirtoCommerce.StoreModule.Data/Migrations/20190522054351_InitialStore.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace VirtoCommerce.StoreModule.Data.Migrations
 {
-    public partial class Initial : Migration
+    public partial class InitialStore : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
