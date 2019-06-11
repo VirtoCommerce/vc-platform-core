5d5e7abfea4effde6f4a4123beb677220b0546c4
Andrei Kolchanov <akak1977@users.noreply.github.com>
2019-06-11 12:45:05 +0500

    Export/335 implement exported type metadata provider (#338)
    #335: 1st attempt to implement

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ExportedTypeDefinition.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ExportedTypeDefinition.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace VirtoCommerce.ExportModule.Core.Model
 {
     public class ExportedTypeDefinition
     {
+        public string TypeName { get; set; }
+        public ExportedTypeMetadata[] MetaData { get; set; }
     }
 }
