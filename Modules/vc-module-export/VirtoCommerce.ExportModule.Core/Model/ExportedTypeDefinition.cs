e131ddbe867eba449a256d3baf138f4b570f807b
Andrei Kolchanov <akak1977@users.noreply.github.com>
2019-06-13 15:46:59 +0500

    #335: implement exportedtypemetadata provider (#346)
    Implemented `ExportedTypeMetadata.GetFromType<T>()`

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ExportedTypeDefinition.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ExportedTypeDefinition.cs
@@ -1,8 +1,23 @@
+using System;
+
 namespace VirtoCommerce.ExportModule.Core.Model
 {
     public class ExportedTypeDefinition
     {
         public string TypeName { get; set; }
         public ExportedTypeMetadata MetaData { get; set; }
+        public Func<ExportDataQuery, IPagedDataSource> ExportedDataSourceFactory { get; set; }
+
+        public ExportedTypeDefinition WithDataSourceFactory(Func<ExportDataQuery, IPagedDataSource> factory)
+        {
+            ExportedDataSourceFactory = factory;
+            return this;
+        }
+
+        public ExportedTypeDefinition WithMetadata(ExportedTypeMetadata metadata)
+        {
+            MetaData = metadata;
+            return this;
+        }
     }
 }
