7fe3a848b7cfc3d709bafe04e3f6e9a236063d25
Andrei Kolchanov <akak1977@gmail.com>
2019-06-14 18:13:30 +0500

    Review comments accepted

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/BaseExportPagedDataSource.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/BaseExportPagedDataSource.cs
@@ -17,7 +17,7 @@ namespace VirtoCommerce.ExportModule.Core.Model
             }
         }
 
-        public int PageSize { get; private set; }
+        public int PageSize { get; set; }
         public int CurrentPageNumber { get; private set; }
         public ExportDataQuery DataQuery { get; set; }
         private int _totalCount = -1;
--- Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/IPagedDataSource.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/IPagedDataSource.cs
@@ -4,7 +4,7 @@ namespace VirtoCommerce.ExportModule.Core.Model
 {
     public interface IPagedDataSource
     {
-        int PageSize { get; }
+        int PageSize { get; set; }
         int CurrentPageNumber { get; }
         int GetTotalCount();
         IEnumerable FetchNextPage();
