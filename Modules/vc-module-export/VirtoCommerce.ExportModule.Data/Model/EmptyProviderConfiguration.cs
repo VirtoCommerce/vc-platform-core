8b1428ed61c3ab6e423702d8ec73c66ef5bd8de7
Andrey Kozlov <yecli5@gmail.com>
2019-06-20 18:44:41 +0500

    Export 347 export module api (#366)
    #347: Imlemented API;
    - Reworked ExportProviderFactory;

---
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Data/Model/EmptyProviderConfiguration.cs
@@ -0,0 +1,8 @@
+using VirtoCommerce.ExportModule.Core.Model;
+
+namespace VirtoCommerce.ExportModule.Data.Model
+{
+    public class EmptyProviderConfiguration : IExportProviderConfiguration
+    {
+    }
+}
