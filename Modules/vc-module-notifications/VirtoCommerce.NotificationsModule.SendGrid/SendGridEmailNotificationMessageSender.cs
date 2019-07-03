3089344c1866b32c864a6578f0e4c718de99068a
Andrey Kostyrin <kostyrin2@gmail.com>
2019-07-03 07:52:39 +0200

    #379 Notification issues (#388)

--- Modules/vc-module-notifications/VirtoCommerce.NotificationsModule.SendGrid/SendGridEmailNotificationMessageSender.cs
+++ Modules/vc-module-notifications/VirtoCommerce.NotificationsModule.SendGrid/SendGridEmailNotificationMessageSender.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -62,9 +61,9 @@ namespace VirtoCommerce.NotificationsModule.SendGrid
             }
             catch (SmtpException ex)
             {
-                throw new SentNotificationException(ex.Message, ex);
+                throw new SentNotificationException(ex);
             }
-            
+
         }
     }
 }
