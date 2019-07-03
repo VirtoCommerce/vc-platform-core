a0a0e04c01b6095fe14ef47bfccf280a5f96f3a0
Andrey Kozlov <yecli5@gmail.com>
2019-07-03 18:03:54 +0500

    Export 387 different object types in one export file (#397)
    - Used ExportName as column name in CsvProvider, updated tests;
    -Removed different type handling possibility by CsvProvider as mapping becomes too complex;
    - Added test for CsvProvider for same type properies mapping;
    - Minor refactoring for ExportTypeMetadata;

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Tests/ExportedTypeMetadataTests.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Tests/ExportedTypeMetadataTests.cs
@@ -8,7 +8,7 @@ namespace VirtoCommerce.ExportModule.Tests
     public class ExportedTypeMetadataTests
     {
         [Fact]
-        public async Task GetFromType_Pricelist_BuiltCorrectly()
+        public async Task GetFromType_Pricelist_NameBuiltCorrectly()
         {
             var metadata = ExportedTypeMetadata.GetFromType<Pricelist>();
             var props = metadata.PropertiesInfo.Select(x => x.Name);
@@ -23,12 +23,55 @@ namespace VirtoCommerce.ExportModule.Tests
             Assert.Contains("ModifiedBy", props);
             Assert.Contains("ShouldSerializeAuditableProperties", props);
             Assert.Contains("Id", props);
+            Assert.Contains("Prices.Id", props);
+            Assert.Contains("Assignments.Id", props);
+
 
             // Check it doesn't contains recursive links
             Assert.DoesNotContain("Prices.Pricelist", props);
             Assert.DoesNotContain("Prices.Pricelist.Id", props);
             Assert.DoesNotContain("Assignments.Pricelist", props);
+            Assert.DoesNotContain("Assignments", props);
+        }
 
+        [Fact]
+        public async Task GetFromType_Pricelist_ExportNameBuiltCorrectly()
+        {
+            var metadata = ExportedTypeMetadata.GetFromType<Pricelist>();
+            var props = metadata.PropertiesInfo.Select(x => x.ExportName);
+
+            // Check if all own property detected
+            Assert.Contains("Pricelist.Name", props);
+            Assert.Contains("Pricelist.Description", props);
+            Assert.Contains("Pricelist.Currency", props);
+            Assert.Contains("Pricelist.CreatedDate", props);
+            Assert.Contains("Pricelist.ModifiedDate", props);
+            Assert.Contains("Pricelist.CreatedBy", props);
+            Assert.Contains("Pricelist.ModifiedBy", props);
+            Assert.Contains("Pricelist.ShouldSerializeAuditableProperties", props);
+            Assert.Contains("Pricelist.Id", props);
+
+            // Reference properties
+            Assert.Contains("Pricelist.Prices.Id", props);
+            Assert.Contains("Pricelist.Assignments.Id", props);
+
+            // Check it doesn't contains recursive links
+            Assert.DoesNotContain("Pricelist.Prices.Pricelist", props);
+            Assert.DoesNotContain("Pricelist.Prices.Pricelist.Id", props);
+            Assert.DoesNotContain("Pricelist.Assignments.Pricelist", props);
+            Assert.DoesNotContain("Assignments", props);
+        }
+
+
+        [Fact]
+        public async Task GetFromType_Pricelist_MemberTypeBuiltCorrectly()
+        {
+            var metadata = ExportedTypeMetadata.GetFromType<Pricelist>();
+            var props = metadata.PropertiesInfo;
+
+            Assert.Equal(1, props.Count(x => x.MemberInfo == typeof(Pricelist).GetProperty("Name")));
+            Assert.Equal(1, props.Count(x => x.MemberInfo == typeof(Price).GetProperty("Id")));
+            Assert.Equal(1, props.Count(x => x.MemberInfo == typeof(PricelistAssignment).GetProperty("Id")));
         }
     }
 }
