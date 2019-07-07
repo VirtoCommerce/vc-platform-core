ef92d67d03486ccf2e28fad7bf71f23c51eb7b90
Andrey Nikutin <ani@virtoway.com>
2019-07-07 13:57:39 +0300

    add CartDynamic migration

---
+++ Modules/vc-module-cart/VirtoCommerce.CartModule.Data/Migrations/20190707105554_AddCartDynamicPropertyObjectValue.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.CartModule.Data.Migrations
+{
+    public partial class AddCartDynamicPropertyObjectValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartLineItemDynamicPropertyObjectValue",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 128, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ObjectType = table.Column<string>(maxLength: 256, nullable: true),
+                    ObjectId = table.Column<string>(maxLength: 128, nullable: true),
+                    Locale = table.Column<string>(maxLength: 64, nullable: true),
+                    ValueType = table.Column<string>(maxLength: 64, nullable: false),
+                    ShortTextValue = table.Column<string>(maxLength: 512, nullable: true),
+                    LongTextValue = table.Column<string>(nullable: true),
+                    DecimalValue = table.Column<decimal>(type: "decimal(18,5)", nullable: true),
+                    IntegerValue = table.Column<int>(nullable: true),
+                    BooleanValue = table.Column<bool>(nullable: true),
+                    DateTimeValue = table.Column<DateTime>(nullable: true),
+                    PropertyId = table.Column<string>(maxLength: 128, nullable: true),
+                    DictionaryItemId = table.Column<string>(maxLength: 128, nullable: true),
+                    PropertyName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartLineItemDynamicPropertyObjectValue", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartLineItemDynamicPropertyObjectValue_CartLineItem_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "CartLineItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartPaymentDynamicPropertyObjectValue",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 128, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ObjectType = table.Column<string>(maxLength: 256, nullable: true),
+                    ObjectId = table.Column<string>(maxLength: 128, nullable: true),
+                    Locale = table.Column<string>(maxLength: 64, nullable: true),
+                    ValueType = table.Column<string>(maxLength: 64, nullable: false),
+                    ShortTextValue = table.Column<string>(maxLength: 512, nullable: true),
+                    LongTextValue = table.Column<string>(nullable: true),
+                    DecimalValue = table.Column<decimal>(type: "decimal(18,5)", nullable: true),
+                    IntegerValue = table.Column<int>(nullable: true),
+                    BooleanValue = table.Column<bool>(nullable: true),
+                    DateTimeValue = table.Column<DateTime>(nullable: true),
+                    PropertyId = table.Column<string>(maxLength: 128, nullable: true),
+                    DictionaryItemId = table.Column<string>(maxLength: 128, nullable: true),
+                    PropertyName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartPaymentDynamicPropertyObjectValue", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartPaymentDynamicPropertyObjectValue_CartPayment_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "CartPayment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartShipmentDynamicPropertyObjectValue",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 128, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ObjectType = table.Column<string>(maxLength: 256, nullable: true),
+                    ObjectId = table.Column<string>(maxLength: 128, nullable: true),
+                    Locale = table.Column<string>(maxLength: 64, nullable: true),
+                    ValueType = table.Column<string>(maxLength: 64, nullable: false),
+                    ShortTextValue = table.Column<string>(maxLength: 512, nullable: true),
+                    LongTextValue = table.Column<string>(nullable: true),
+                    DecimalValue = table.Column<decimal>(type: "decimal(18,5)", nullable: true),
+                    IntegerValue = table.Column<int>(nullable: true),
+                    BooleanValue = table.Column<bool>(nullable: true),
+                    DateTimeValue = table.Column<DateTime>(nullable: true),
+                    PropertyId = table.Column<string>(maxLength: 128, nullable: true),
+                    DictionaryItemId = table.Column<string>(maxLength: 128, nullable: true),
+                    PropertyName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartShipmentDynamicPropertyObjectValue", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartShipmentDynamicPropertyObjectValue_CartShipment_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "CartShipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartShoppingCartDynamicPropertyObjectValue",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 128, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ObjectType = table.Column<string>(maxLength: 256, nullable: true),
+                    ObjectId = table.Column<string>(maxLength: 128, nullable: true),
+                    Locale = table.Column<string>(maxLength: 64, nullable: true),
+                    ValueType = table.Column<string>(maxLength: 64, nullable: false),
+                    ShortTextValue = table.Column<string>(maxLength: 512, nullable: true),
+                    LongTextValue = table.Column<string>(nullable: true),
+                    DecimalValue = table.Column<decimal>(type: "decimal(18,5)", nullable: true),
+                    IntegerValue = table.Column<int>(nullable: true),
+                    BooleanValue = table.Column<bool>(nullable: true),
+                    DateTimeValue = table.Column<DateTime>(nullable: true),
+                    PropertyId = table.Column<string>(maxLength: 128, nullable: true),
+                    DictionaryItemId = table.Column<string>(maxLength: 128, nullable: true),
+                    PropertyName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartShoppingCartDynamicPropertyObjectValue", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartShoppingCartDynamicPropertyObjectValue_Cart_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "Cart",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartLineItemDynamicPropertyObjectValue_ObjectId",
+                table: "CartLineItemDynamicPropertyObjectValue",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectType_ObjectId",
+                table: "CartLineItemDynamicPropertyObjectValue",
+                columns: new[] { "ObjectType", "ObjectId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartPaymentDynamicPropertyObjectValue_ObjectId",
+                table: "CartPaymentDynamicPropertyObjectValue",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectType_ObjectId",
+                table: "CartPaymentDynamicPropertyObjectValue",
+                columns: new[] { "ObjectType", "ObjectId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartShipmentDynamicPropertyObjectValue_ObjectId",
+                table: "CartShipmentDynamicPropertyObjectValue",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectType_ObjectId",
+                table: "CartShipmentDynamicPropertyObjectValue",
+                columns: new[] { "ObjectType", "ObjectId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartShoppingCartDynamicPropertyObjectValue_ObjectId",
+                table: "CartShoppingCartDynamicPropertyObjectValue",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectType_ObjectId",
+                table: "CartShoppingCartDynamicPropertyObjectValue",
+                columns: new[] { "ObjectType", "ObjectId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartLineItemDynamicPropertyObjectValue");
+
+            migrationBuilder.DropTable(
+                name: "CartPaymentDynamicPropertyObjectValue");
+
+            migrationBuilder.DropTable(
+                name: "CartShipmentDynamicPropertyObjectValue");
+
+            migrationBuilder.DropTable(
+                name: "CartShoppingCartDynamicPropertyObjectValue");
+        }
+    }
+}
