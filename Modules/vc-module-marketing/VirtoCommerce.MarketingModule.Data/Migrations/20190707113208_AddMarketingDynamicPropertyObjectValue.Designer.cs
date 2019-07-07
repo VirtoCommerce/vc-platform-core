23fcb4e6f54802c554c5f619a0d0727340536ad9
Andrey Nikutin <ani@virtoway.com>
2019-07-07 14:32:44 +0300

    add marketingDynamic migration

--- Modules/vc-module-marketing/VirtoCommerce.MarketingModule.Data/Migrations/20190707113208_AddMarketingDynamicPropertyObjectValue.Designer.cs
+++ Modules/vc-module-marketing/VirtoCommerce.MarketingModule.Data/Migrations/20190707113208_AddMarketingDynamicPropertyObjectValue.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.MarketingModule.Data.Repositories;
 
 namespace VirtoCommerce.MarketingModule.Data.Migrations
 {
     [DbContext(typeof(MarketingDbContext))]
-    partial class MarketingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190707113208_AddMarketingDynamicPropertyObjectValue")]
+    partial class AddMarketingDynamicPropertyObjectValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
