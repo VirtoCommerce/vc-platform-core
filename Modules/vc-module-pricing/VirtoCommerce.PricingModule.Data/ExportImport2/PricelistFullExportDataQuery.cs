329a9b96611728291981e4356d385c57a1c67247
Andrei Kolchanov <akak1977@gmail.com>
2019-07-24 13:11:14 +0500

    Default IncludedColumns initialization in ****ExportDataQuery does not need anymore.

--- Modules/vc-module-pricing/VirtoCommerce.PricingModule.Data/ExportImport2/PricelistFullExportDataQuery.cs
+++ Modules/vc-module-pricing/VirtoCommerce.PricingModule.Data/ExportImport2/PricelistFullExportDataQuery.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using VirtoCommerce.ExportModule.Core.Model;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.PricingModule.Core.Model.Search;
@@ -7,22 +6,6 @@ namespace VirtoCommerce.PricingModule.Data.ExportImport
 {
     public class PricelistFullExportDataQuery : ExportDataQuery
     {
-        public PricelistFullExportDataQuery()
-        {
-            IncludedColumns = new[]
-            {
-                "Id", "Name", "Currency", "Description",
-                "Assignments.Id", "Assignments.CatalogId", "Assignments.Name", "Assignments.Priority", "Assignments.ConditionExpression","Assignments.PredicateVisualTreeSerialized",
-                "Prices.Id", "Prices.Currency", "Prices.ProductId", "Prices.Sale", "Prices.List", "Prices.MinQuantity","Prices.StartDate", "Prices.EndDate"
-            }
-            .Select(x => new ExportedTypeColumnInfo()
-            {
-                Name = x,
-                ExportName = x,
-            })
-            .ToArray();
-        }
-
         public override SearchCriteriaBase CreateSearchCriteria()
         {
             return new PricelistSearchCriteria();
