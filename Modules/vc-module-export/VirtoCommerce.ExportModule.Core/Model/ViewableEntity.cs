0f269ffa66baee240c478e04edfc97744d7063f6
Andrey Kozlov <yecli.github@gmail.com>
2019-07-26 15:37:45 +0500

    #458: Added Parent and Type to ViewableEntity; (#462)
    Added Parent and Type to ViewableEntity;
    Fixed ExportPriceCSV test;

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ViewableEntity.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ViewableEntity.cs
@@ -7,5 +7,7 @@ namespace VirtoCommerce.ExportModule.Core.Model
         public string Name { get; set; }
         public string Code { get; set; }
         public string ImageUrl { get; set; }
+        public string Parent { get; set; }
+        public string Type { get; set; }
     }
 }
