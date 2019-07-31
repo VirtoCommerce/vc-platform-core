8bd490e41342da2556b5e98f1d1a35dedcb7cf68
Andrei Kolchanov <akak1977@gmail.com>
2019-07-31 11:32:38 +0500

    Column selector reworked

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Web/Scripts/blades/export-columns-selector.js
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Web/Scripts/blades/export-columns-selector.js
@@ -2,12 +2,16 @@ angular.module('virtoCommerce.exportModule')
     .controller('virtoCommerce.exportModule.exportColumnsSelectorController', ['$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
         var blade = $scope.blade;
 
+        blade.isLoading = true;
+
         function initializeBlade() {
             blade.title = 'export.blades.export-settings.labels.exported-columns';
             blade.headIcon = 'fa-folder';
 
             var allColumns = angular.copy(blade.exportDataRequest.allColumnsOfType);
+            allColumns = _.sortBy(allColumns, 'group', 'name');
             var selectedColumns = angular.copy(blade.exportDataRequest.dataQuery.includedColumns);
+            selectedColumns = _.sortBy(selectedColumns, 'name');
             blade.allEntities = _.groupBy(allColumns, 'group');
             blade.selectedEntities = _.groupBy(selectedColumns, 'group');
             blade.isLoading = false;
@@ -19,6 +23,10 @@ angular.module('virtoCommerce.exportModule')
 
         $scope.clearAllInGroup = function (groupKey) {
             blade.selectedEntities[groupKey] = [];
+        }
+
+        $scope.sortSelected = function (groupKey) {
+            blade.selectedEntities[groupKey] = _.sortBy(blade.selectedEntities[groupKey], 'name');
         }
 
         $scope.cancelChanges = function () {
