d976e52e03a32fe6588944b78638f6413f3bf21a
Andrey Kozlov <yecli.github@gmail.com>
2019-08-14 13:17:57 +0500

    Revert "Revert "vp 254 xml comments (#513)" (#514)" (#515)
    This reverts commit fa9c49645aa71c256bad6f7f94dbf914a943967c.

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ExportedTypeDefinition.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Core/Model/ExportedTypeDefinition.cs
@@ -5,43 +5,42 @@ using VirtoCommerce.ExportModule.Core.Services;
 namespace VirtoCommerce.ExportModule.Core.Model
 {
     /// <summary>
-    /// Definition of single exported entity type
+    /// Definition of exported entity type
     /// </summary>
     public class ExportedTypeDefinition
     {
         /// <summary>
-        /// Logical type name, given during registration. Commonly non-equal to <see cref="ExportableEntity{T}"/> descendants type names.
-        /// Users can see it in type selector blade.
+        /// Logical type name, given during registration. It could be non-equal to exportable type name.
         /// </summary>
         public string TypeName { get; set; }
 
         /// <summary>
-        /// Logical group name. Definitions with the same group names fall into the folder in type selector blade.
+        /// Logical group name. Entity types can be divided into different groups to simplify selection.
         /// </summary>
         public string Group { get; set; }
 
         /// <summary>
-        /// Metadata for this definition (set of columns, version)
+        /// Metadata for this definition (set of properties, version)
         /// </summary>
         public ExportedTypeMetadata MetaData { get; set; }
 
         /// <summary>
-        /// Metadata for this definition in the case of supported tabular export (flat files, like *.csv).
+        /// Metadata for this definition in case of supported tabular export (flat files, like *.csv).
         /// </summary>
         public ExportedTypeMetadata TabularMetaData { get; set; }
 
         /// <summary>
-        /// Type name for <see cref="ExportDataQuery"/> descendand. This used to instantiate data query of this type at export start.
+        /// Specific type name with which we could query exported type data.
         /// </summary>
         public string ExportDataQueryType { get; set; }
 
         /// <summary>
-        /// Returns <see cref="true"/> if tabular export supported, <see cref="TabularDataConverter"/> is set. 
+        /// Returns <see cref="true"/> if tabular export supported, <see cref="TabularDataConverter"/> is set        
         /// </summary>
         public bool IsTabularExportSupported { get => TabularDataConverter != null; }
 
         /// <summary>
-        /// Converter for transforming <see cref="ExportableEntity{T}"/> to tabular form
+        /// Converter for transforming exportable entity to tabular representation
         /// </summary>
         [JsonIgnore]
         public ITabularDataConverter TabularDataConverter { get; set; }
