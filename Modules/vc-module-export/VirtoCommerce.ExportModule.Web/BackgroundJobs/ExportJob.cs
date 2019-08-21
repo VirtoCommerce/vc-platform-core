f19f014476d6c7ae6204c6003a71373eb43ccb32
Andrey Kozlov <yecli.github@gmail.com>
2019-08-21 21:50:25 +0500

    VP-299: Added "Export.General.FileNameTemplate" setting to manage export file name template. (#535)
    Added "Export.General.FileNameTemplate" setting to manage export file name template.

--- Modules/vc-module-export/VirtoCommerce.ExportModule.Web/BackgroundJobs/ExportJob.cs
+++ Modules/vc-module-export/VirtoCommerce.ExportModule.Web/BackgroundJobs/ExportJob.cs
@@ -4,11 +4,13 @@ using System.Threading.Tasks;
 using Hangfire;
 using Hangfire.Server;
 using Microsoft.Extensions.Options;
+using VirtoCommerce.ExportModule.Core;
 using VirtoCommerce.ExportModule.Core.Model;
 using VirtoCommerce.ExportModule.Core.Services;
 using VirtoCommerce.Platform.Core;
 using VirtoCommerce.Platform.Core.Exceptions;
 using VirtoCommerce.Platform.Core.PushNotifications;
+using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.ExportModule.Web.BackgroundJobs
 {
@@ -18,16 +20,34 @@ namespace VirtoCommerce.ExportModule.Web.BackgroundJobs
         private readonly PlatformOptions _platformOptions;
         private readonly IDataExporter _dataExporter;
         private readonly IExportProviderFactory _exportProviderFactory;
+        private readonly ISettingsManager _settingsManager;
+
+        private string fileNameTemplate;
 
         public ExportJob(IDataExporter dataExporter,
             IPushNotificationManager pushNotificationManager,
             IOptions<PlatformOptions> platformOptions,
-            IExportProviderFactory exportProviderFactory)
+            IExportProviderFactory exportProviderFactory,
+            ISettingsManager settingsManager)
         {
             _dataExporter = dataExporter;
             _pushNotificationManager = pushNotificationManager;
             _platformOptions = platformOptions.Value;
             _exportProviderFactory = exportProviderFactory;
+            _settingsManager = settingsManager;
+        }
+
+        private string FileNameTemplate
+        {
+            get
+            {
+                if (fileNameTemplate == null)
+                {
+                    fileNameTemplate = _settingsManager.GetValue(ModuleConstants.Settings.General.ExportFileNameTemplate.Name, ModuleConstants.Settings.General.ExportFileNameTemplate.DefaultValue.ToString());
+                }
+
+                return fileNameTemplate;
+            }
         }
 
         public async Task ExportBackgroundAsync(ExportDataRequest request, ExportPushNotification notification, IJobCancellationToken cancellationToken, PerformContext context)
@@ -42,7 +62,7 @@ namespace VirtoCommerce.ExportModule.Web.BackgroundJobs
             try
             {
                 var localTmpFolder = Path.GetFullPath(Path.Combine(_platformOptions.DefaultExportFolder));
-                var fileName = Path.GetFileName(_platformOptions.DefaultExportFileName);
+                var fileName = string.Format(FileNameTemplate, DateTime.UtcNow);
 
                 // Do not like provider creation here to get file extension, maybe need to pass created provider to Exporter.
                 // Create stream inside Exporter is not good as it is not Exporter resposibility to decide where to write.
@@ -50,7 +70,7 @@ namespace VirtoCommerce.ExportModule.Web.BackgroundJobs
 
                 if (!string.IsNullOrEmpty(provider.ExportedFileExtension))
                 {
-                    fileName = Path.ChangeExtension(Path.GetFileName(_platformOptions.DefaultExportFileName), provider.ExportedFileExtension);
+                    fileName = Path.ChangeExtension(fileName, provider.ExportedFileExtension);
                 }
 
                 var localTmpPath = Path.Combine(localTmpFolder, fileName);
