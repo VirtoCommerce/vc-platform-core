2f52d79a4c6679ad9cddde2d8a7ad9b1dfff0b21
Andrei Kostyrin <kostyrin2@gmail.com>
2019-09-02 16:06:47 +0200

    #518 Added condition for migration a new module

--- Modules/vc-module-tax/VirtoCommerce.TaxModule.Data/Migrations/20000000000000_UpdateTaxV2.cs
+++ Modules/vc-module-tax/VirtoCommerce.TaxModule.Data/Migrations/20000000000000_UpdateTaxV2.cs
@@ -6,9 +6,8 @@ namespace VirtoCommerce.TaxModule.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"IF (EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_NAME = '__MigrationHistory'))
+            migrationBuilder.Sql(@"IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '__MigrationHistory'))
+                IF (EXISTS (SELECT * FROM __MigrationHistory WHERE ContextKey = 'VirtoCommerce.StoreModule.Data.Migrations.Configuration'))
                     BEGIN
 	                    INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId],[ProductVersion]) VALUES ('20190409093711_InitialTax', '2.2.3-servicing-35854')
                         ALTER TABLE [StoreTaxProvider] ADD [TypeName] nvarchar(128) NOT NULL Default ('')
